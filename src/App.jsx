9963f293df09f64f157a19a28d7a13723151039d
akshaychaturvedi <akshaychaturvedi129@gmail.com>
2025-06-18 19:15:16 +0530

    change language

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useTranslation } from 'react-i18next';
 import './utils/i18n/index';
+import { useEffect } from 'react';
+import i18n from './utils/i18n/index';
 
 function App() {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage(navigator.language)
+  }, [])
 
   return (
     <>
